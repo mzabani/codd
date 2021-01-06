25c289715f7c25259642522a6b5ad12b97116f4e
Marcelo Zabani <mzabani@gmail.com>
2021-01-06 10:26:21 -0300

    Previously undetected cases now successfully parsed (#12)
    * Previously undetected cases now successfully parsed
    
    * Parsing SELECT vs other statements

--- src/Codd/Internal/MultiQueryStatement.hs
+++ src/Codd/Internal/MultiQueryStatement.hs
@@ -1,11 +1,10 @@
-module Codd.Internal.MultiQueryStatement (mqStatement_, parseMultiStatement, parseMultiStatementDetailed) where
+module Codd.Internal.MultiQueryStatement (SqlBlock(..), SqlStatement(..), blockToText, finalCommentsTextOnly, noTxnStatement_, noTxnSingleStatement_, inTxnStatement_, parseMultiStatement, statementTextOnly) where
 
 import Prelude hiding (takeWhile)
 
-import Codd.Query (execvoid_)
 import Control.Applicative ((<|>))
-import Control.Monad (void, forM_)
-import Data.Attoparsec.Text (Parser, anyChar, atEnd, char, endOfInput, many1, parseOnly, peekChar, string, takeWhile)
+import Control.Monad (void, forM_, guard)
+import Data.Attoparsec.Text (Parser, anyChar, atEnd, char, endOfInput, many', many1, parseOnly, peekChar, string, takeWhile, takeWhile1)
 import qualified Data.Attoparsec.Text as Parsec
 import qualified Data.Char as Char
 import qualified Data.Text as Text
@@ -26,65 +25,101 @@ import UnliftIO (MonadIO, liftIO)
 -- (psql counts parentheses, for instance) to split them up by that.
 -- Note 2: The CLI utility psql does the same and splits up commands before sending them to the server. See https://github.com/postgres/postgres/blob/master/src/fe_utils/psqlscan.l
 
-mqStatement_ :: MonadIO m => DB.Connection -> Text -> m ()
-mqStatement_ conn q =
+noTxnStatement_ :: MonadIO m => DB.Connection -> Text -> m ()
+noTxnStatement_ conn q =
     -- Fallback into regular command in case of parsing error
-    case parseOnly (multiStatementParser <* endOfInput) q of
-        Left _ -> singleStatementExec
+    case parseMultiStatement q of
+        Left _ -> parseErrorFallbackExec
         Right stms ->
-            if Text.concat stms /= q then
-                singleStatementExec
+            if Text.concat (map blockToText stms) /= q then
+                parseErrorFallbackExec
             else
-                forM_ stms $ \sql -> execvoid_ conn (DB.Query $ encodeUtf8 sql)
+                forM_ stms $ \sql -> noTxnSingleStatement_ conn sql
 
     where
-        singleStatementExec = do
-            liftIO $ putStrLn $ "NOTE: An internal inconsistency was detected in the multi statement parser. You should receive an error when adding this migration if this would mean an error when running it, so it shouldn't be a problem. Still, please report this as a bug."
-            execvoid_ conn $ DB.Query (encodeUtf8 q)
+        parseErrorFallbackExec = do
+            liftIO $ putStrLn $ "NOTE: An internal inconsistency was detected in the multi statement parser. We now have to run this migration inside a transaction, so it may fail. Please report this as a bug."
+            inTxnStatement_ conn $ DB.Query (encodeUtf8 q)
 
-parseMultiStatement :: Text -> Either String [Text]
-parseMultiStatement = parseOnly (multiStatementParser <* endOfInput) 
+-- | This is sad, but when running each command separately, there'll be row-returning statements such as SELECT as well
+-- as Int64 returning ones such as INSERT, ALTER TABLE etc..
+noTxnSingleStatement_ :: MonadIO m => DB.Connection -> SqlBlock -> m ()
+noTxnSingleStatement_ conn (SqlBlock (SelectStatement s) comm) = liftIO $ void $ DB.query_ @(DB.Only Int) conn $ DB.Query $ encodeUtf8 $ s <> comm <> "\n;SELECT 1;"
+noTxnSingleStatement_ conn (SqlBlock (OtherStatement s) comm) = liftIO $ void $ DB.execute_ conn $ DB.Query $ encodeUtf8 $ s <> comm
 
-parseMultiStatementDetailed :: Text -> Either String [(Text, Text)]
-parseMultiStatementDetailed = parseOnly (multiStatementParserDetailed <* endOfInput) 
+inTxnStatement_ :: MonadIO m => DB.Connection -> DB.Query -> m ()
+inTxnStatement_ conn s =
+    -- Same problem as noTxnSingleStatement_, but if we're in a in-txn migration, we can append a "SELECT 1" and we're good. No need to parse things.
+    liftIO $ void $ DB.query_ @(DB.Only Int) conn $ s <> "\n;SELECT 1;"
 
-multiStatementParser :: Parser [Text]
-multiStatementParser = do
-    parsedStatements <- multiStatementParserDetailed
-    pure $ map (\(t1, t2) -> t1 <> t2) parsedStatements
+parseMultiStatement :: Text -> Either String [SqlBlock]
+parseMultiStatement = parseOnly (multiStatementParserDetailed <* endOfInput) 
 
-multiStatementParserDetailed :: Parser [(Text, Text)]
+multiStatementParserDetailed :: Parser [SqlBlock]
 multiStatementParserDetailed = many1 singleStatementParser
 
--- | Parses statements into the actual SQL statement first and comments+whitespace second.
-singleStatementParser :: Parser (Text, Text)
-singleStatementParser = do
-    t1 <- takeWhile (\c -> not (isPossibleStartingChar c) && c /= ';')
-    mc <- peekChar
-    case mc of
-        Nothing -> if t1 == "" then fail "Empty input" else pure (t1, "") -- This could be a single statement without a semi-colon
-        Just ';' -> do
-            void $ char ';'
-            -- To avoid trailing comments being parsed as a separate command, look for comments or space now as well
-            trailingCommentOrSpace <- (Text.concat <$> many1 commentParser) <|> takeWhile Char.isSpace <|> pure ""
-            pure (Text.snoc t1 ';', trailingCommentOrSpace)
-        Just _ -> do
-            (_, t2) <- blockParser
-            -- After reading an entire block, we still need to find a semi-colon to get a statement from start to finish!
-            -- One exception: eof
-            done <- atEnd
-            if done then pure (t1 <> t2, "") -- This could be a single statement without a semi-colon
-            else do
-                (more1, endingBlock) <- singleStatementParser
-                pure (t1 <> t2 <> more1, endingBlock)
+data SqlStatement = SelectStatement Text | OtherStatement Text
+    deriving stock (Show, Eq)
 
-commentParser :: Parser Text
-commentParser = do
-    s1 <- takeWhile Char.isSpace
-    (commentType, commentInit) <- (DoubleDashComment,) <$> string "--" <|> (CStyleComment,) <$> string "/*"
-    bRemaining <- blockInnerContentsParser commentType
-    s2 <- takeWhile Char.isSpace
-    pure $ s1 <> commentInit <> bRemaining <> s2
+-- | SqlBlock is a statement succeeded by some comment and white-space (or empty Text if none).
+data SqlBlock = SqlBlock SqlStatement Text
+    deriving stock (Show, Eq)
+
+blockToText :: SqlBlock -> Text
+blockToText (SqlBlock (SelectStatement s) comm) = s <> comm
+blockToText (SqlBlock (OtherStatement s) comm) = s <> comm
+
+statementTextOnly :: SqlBlock -> Text
+statementTextOnly (SqlBlock (SelectStatement s) _) = s
+statementTextOnly (SqlBlock (OtherStatement s) _) = s
+
+finalCommentsTextOnly :: SqlBlock -> Text
+finalCommentsTextOnly (SqlBlock _ c) = c
+
+-- | Parses statements into the actual SQL statement first and comments+whitespace second.
+singleStatementParser :: Parser SqlBlock
+singleStatementParser = do
+    -- 0 or more comments and white-space
+    commentsBefore <- Text.concat <$> many' commentOrSpaceParser
+    
+    -- 0 or more blocks of any type, until a semi-colon or eof
+    stm <- blocksUntilEofOrSemiColon
+    guard $ stm /= ""
+
+    -- 0 or more comments and white-space
+    commentsAfter <- Text.concat <$> many' commentOrSpaceParser
+    pure $ sectionsToBlock commentsBefore stm commentsAfter
+
+    where
+        blocksUntilEofOrSemiColon = do
+            t1 <- takeWhile (\c -> not (isPossibleStartingChar c) && c /= ';')
+            mc <- peekChar
+            case mc of
+                Nothing -> pure t1
+                Just ';' -> do
+                    void $ char ';'
+                    pure $ Text.snoc t1 ';'
+                Just _ -> do
+                    t2 <- Text.concat . map snd <$> many1 blockParser <|> Parsec.take 1
+                    -- After reading blocks or just the char, we still need to find a semi-colon to get a statement from start to finish!
+                    t3 <- blocksUntilEofOrSemiColon
+                    pure $ t1 <> t2 <> t3
+                    
+        sectionsToBlock :: Text -> Text -> Text -> SqlBlock
+        sectionsToBlock befStm stm aftStm =
+            case Text.toCaseFold (Text.takeWhile (not . Char.isSpace) stm) of
+                "select" -> SqlBlock (SelectStatement (befStm <> stm)) aftStm
+                _ -> SqlBlock (OtherStatement (befStm <> stm)) aftStm
+
+commentOrSpaceParser :: Parser Text
+commentOrSpaceParser = commentParser <|> takeWhile1 Char.isSpace
+    where
+        commentParser = do
+            s1 <- takeWhile Char.isSpace
+            (commentType, commentInit) <- (DoubleDashComment,) <$> string "--" <|> (CStyleComment,) <$> string "/*"
+            bRemaining <- blockInnerContentsParser commentType
+            s2 <- takeWhile Char.isSpace
+            pure $ s1 <> commentInit <> bRemaining <> s2
 
 data BlockType = DoubleDashComment | CStyleComment | DollarQuotedBlock Text | DoubleQuotedIdentifier | SingleQuotedString deriving stock Show
 
