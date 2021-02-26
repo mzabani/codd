b8c903f8a59b4758311fd2107a61f7532b522746
Marcelo Zabani <mzabani@gmail.com>
2021-02-26 18:02:46 -0300

    Fixing wipeDir

--- test/GeneralSpec.hs
+++ test/GeneralSpec.hs
@@ -1,12 +1,15 @@
 module GeneralSpec where
 
-import Codd.Hashing (readHashesFromDisk, persistHashesToDisk)
-import Control.Monad (when)
-import Data.Text (unpack)
-import Test.Hspec
-import Test.Hspec.Core.QuickCheck (modifyMaxSuccess)
-import Test.QuickCheck
-import TypesGen (DbHashesGen(..))
+import           Codd.Hashing                   ( hashDifferences
+                                                , persistHashesToDisk
+                                                , readHashesFromDisk
+                                                )
+import           Control.Monad                  ( when )
+import           Data.Text                      ( unpack )
+import           Test.Hspec
+import           Test.Hspec.Core.QuickCheck     ( modifyMaxSuccess )
+import           Test.QuickCheck
+import           TypesGen                       ( DbHashesGen(..) )
 
 spec :: Spec
 spec = do
@@ -14,16 +17,17 @@ spec = do
         it "persistHashesToDisk is inverse of readHashesFromDisk" $ do
             property $ \(DbHashesGen dbHashes) -> do
                 persistHashesToDisk dbHashes "/dev/shm/inverse-test-sql-folder"
-                readDbHashes <- readHashesFromDisk "/dev/shm/inverse-test-sql-folder"
-                when (readDbHashes /= dbHashes) $ do
-                    putStrLn "Hashes differ!"
-                    putStrLn "Randomly generated hashes (expected):"
-                    print dbHashes
-                    putStrLn "Hashes read from disk"
-                    print readDbHashes
-                    error "Failed!"
-        modifyMaxSuccess (const 1) $
-            it "persistHashesToDisk works even when temporary folder and final folder are in separate filesystems" $ do
+                readDbHashes <- readHashesFromDisk
+                    "/dev/shm/inverse-test-sql-folder"
+                let diffs = hashDifferences dbHashes readDbHashes
+                diffs `shouldBe` []
+                readDbHashes `shouldBe` dbHashes
+        modifyMaxSuccess (const 1)
+            $ it
+                  "persistHashesToDisk works even when temporary folder and final folder are in separate filesystems"
+            $ do
                 -- We don't know where the temporary folder will be, but it'll either be in /dev/shm or it won't,
                 -- and /dev/shm is (hopefully) always a separately mounted tmpfs
-                property $ \(DbHashesGen dbHashes) -> persistHashesToDisk @IO dbHashes "/tmp/some-or-any-codd-folder"
+                  property $ \(DbHashesGen dbHashes) -> persistHashesToDisk @IO
+                      dbHashes
+                      "/tmp/some-or-any-codd-folder"
