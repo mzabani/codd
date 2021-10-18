9726c510ae6a820cfd18b9f74dc227f795e11993
Marcelo Zabani <mzabani@gmail.com>
2021-10-18 16:53:48 -0300

    Nix and CI changes (#67)
    * Changing order of CI steps and pure test shells
    
    * Postgres wipes cluster for tests

--- nix/test-shell-pg.nix
+++ nix/test-shell-pg.nix
@@ -2,7 +2,7 @@
 , postgres
 , pgDataDir }:
     let
-        postgres-service = import ./postgres-service.nix { inherit postgres; inherit pkgs; };
+        postgres-service = import ./postgres-service.nix { inherit postgres; inherit pkgs; wipeCluster = true; };
     in
     pkgs.mkShell {
         buildInputs = [ postgres-service pkgs.glibcLocales ];
