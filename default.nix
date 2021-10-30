3adcb98db394467a92a275aaf9b92bfce341645c
Marcelo Zabani <mzabani@gmail.com>
2021-10-30 19:20:23 -0300

    Work around hspec-discover issue with haskell.nix (#70)

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import ./nix/nixpkgs.nix }:
   with pkgs.haskell-nix;
-  stackProject {
+  let pkgSet = stackProject {
       # 'cleanGit' cleans a source directory based on the files known by git
       src = pkgs.haskell-nix.haskellLib.cleanGit {
         name = "codd";
@@ -14,4 +14,18 @@
           packages.postgresql-libpq.components.library.build-tools = [ pkgs.postgresql ];
         }
       ];
+  };
+  in
+  # Work around https://github.com/input-output-hk/haskell.nix/issues/231. More info
+  # in package.yaml
+  pkgSet // {
+    codd = pkgSet.codd // {
+      components = pkgSet.codd.components // {
+        tests = pkgSet.codd.components.tests // {
+          codd-test = pkgSet.codd.components.tests.codd-test.overrideAttrs (_: {
+            depsBuildBuildPropagated = [ pkgSet.hspec-discover.components.exes.hspec-discover ];
+          });
+        };
+      };
+    };
   }
