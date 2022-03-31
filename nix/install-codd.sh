4ce7539e1f9180d584a4177981138a06bf9410fe
Marcelo Zabani <mzabani@gmail.com>
2022-03-31 18:14:04 -0300

    Fixes installation with Nix < 2.4

--- nix/install-codd.sh
+++ nix/install-codd.sh
@@ -3,14 +3,21 @@
 # This installs codd with nix-env and good caching set up
 # Note that:
 # 1. Github's auto generated master.tar.gz archive does not include submodules.
-# 2. nix-env interprets the "-f" flag differently depending on whether it's a URL or
+# 2. nix-env and nix-shell both interpret the file path differently depending on whether it's a URL or
 #    a local file path:
 #    "If the argument starts with http:// or https://, it is interpreted as the URL of a tarball that will be downloaded and  unpacked
 #     to a temporary location"
+# 3. I could not find a way to use Nix's fetchgit* functions to fetch `master` impurely.
+#    builtins.fetchGit does it, but doesn't allow submodules to be fetched for Nix <2.4.
 
-curl https://raw.githubusercontent.com/mzabani/codd/master/nix/codd-master-default.nix -o codd-master-default-TEMPORARY-FILE.nix
-trap "rm codd-master-default-TEMPORARY-FILE.nix" EXIT
-nix-env -f "./codd-master-default-TEMPORARY-FILE.nix" \
+trap "rm -f install-codd-nixpkgs-TEMPORARY-FILE.nix" EXIT
+curl https://raw.githubusercontent.com/mzabani/codd/master/nix/install-codd-nixpkgs.nix -o install-codd-nixpkgs-TEMPORARY-FILE.nix
+
+# To test this script, create a new directory and use the line below instead of "curl".
+# cp ~/Projects/codd/nix/install-codd-nixpkgs.nix install-codd-nixpkgs-TEMPORARY-FILE.nix
+
+CODDPATH=$(nix-shell -E "let n = import ./install-codd-nixpkgs-TEMPORARY-FILE.nix; in n.installShell" --run "nix-prefetch-git --quiet --fetch-submodules https://github.com/mzabani/codd.git refs/heads/master | jq -r .path")
+nix-env -f "$CODDPATH" \
     --option trusted-substituters 'https://cache.nixos.org https://hydra.iohk.io https://mzabani.cachix.org' \
     --option trusted-public-keys  'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= mzabani.cachix.org-1:wnkKakfl+rbT7zTtV1P1tAtjBTuh6GQVX7LfSd9sMbA=' \
     -iA codd.components.exes.codd
--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,19 +1,7 @@
 let
-  # Fetch the latest haskell.nix and import its default.nix
-  haskellNix = import (builtins.fetchTarball {
-    url =
-      "https://github.com/input-output-hk/haskell.nix/archive/b2479523c0bafc9a6ce63cafefd38a110dd01331.tar.gz";
-    sha256 = "0i77m8348klzqh2hry7gmykmkk3ngdnabnfjja2mwpynazpgvvzh";
-  }) { };
-
-  # haskell.nix provides access to the nixpkgs pins which are used by our CI,
-  # hence you will be more likely to get cache hits when using these.
-  # But you can also just use your own, e.g. '<nixpkgs>'.
-  nixpkgsSrc = haskellNix.sources.nixpkgs-unstable;
-
-  # haskell.nix provides some arguments to be passed to nixpkgs, including some
-  # patches and also the haskell.nix functionality itself as an overlay.
-  nixpkgsArgs = haskellNix.nixpkgsArgs;
+  pkgsAttrs = import ./install-codd-nixpkgs.nix;
+  nixpkgsSrc = pkgsAttrs.nixpkgsSrc;
+  nixpkgsArgs = pkgsAttrs.nixpkgsArgs;
 
   noKvmOverlay = import ./docker/no-kvm-overlay.nix;
   customArgs = nixpkgsArgs // {
