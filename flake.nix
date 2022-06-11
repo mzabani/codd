7f641350f327000e677fdc8b2f2b4b5198698a0c
Marcelo Zabani <mzabani@gmail.com>
2022-06-11 15:25:34 -0300

    hspec-discover working

--- flake.nix
+++ flake.nix
@@ -19,46 +19,62 @@
         };
         overlays = [
           haskellNix.overlay
-          (final: prev: {
-            # This overlay adds our project to pkgs
-            coddProject = final.haskell-nix.project' {
-              src = ./.;
-              compiler-nix-name = "ghc8107";
-              # This is used by `nix develop .` to open a shell for use with
-              # `cabal`, `hlint` and `haskell-language-server`
-              shell.tools = {
-                cabal = "latest";
-                hlint = "latest";
-                haskell-language-server = "latest";
+          (final: prev:
+            let finalIohkPkgs = final.haskell-nix.haskellPackages;
+            in {
+              # This overlay adds our project to pkgs
+              coddProject = final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc8107";
+
+                modules = [{
+                  # Musl builds fail because postgresql-libpq requires pg_config in the path for its configure phase.
+                  # See https://github.com/haskellari/postgresql-libpq/blob/master/Setup.hs#L65-L66
+                  packages.postgresql-libpq.components.library.build-tools =
+                    [ pkgs.postgresql ];
+
+                  # Work around https://github.com/input-output-hk/haskell.nix/issues/231. More info
+                  # in package.yaml
+                  packages.codd.components.tests.codd-test.build-tools = [
+                    finalIohkPkgs.hspec-discover.components.exes.hspec-discover
+                  ];
+                }];
+
+                # This is used by `nix develop .` to open a shell for use with
+                # `cabal`, `hlint` and `haskell-language-server`
+                shell.tools = {
+                  cabal = "latest";
+                  hlint = "latest";
+                  haskell-language-server = "latest";
+                };
+                # Non-Haskell shell tools go here
+                shell.buildInputs = with pkgs; [
+                  ghcid
+                  hpack
+                  haskellPackages.brittany # Brittany from the LTS is older than this
+                  # finalIohkPkgs.brittany.components.exes.brittany
+                  postgres
+                  glibcLocales
+                  cacert
+                  postgres-service
+                ];
+                shell.shellHook = ''
+                  source scripts/source-env.sh .env
+
+                  # init-postgres doesn't actually work with direnv. I tried to daemonize starting postgres but was not able
+                  # to make it work. See https://github.com/direnv/direnv/issues/755
+                  init-postgres
+
+                  echo You should be able to start postgres with 'pg_ctl start' and use 'psql' to connect to it, and it will be independent from any your own system might have provided.
+                  echo You just might have to run ./scripts/create-dev-db.sh and then 'codd.sh up' first to create database $PGDATABASE.
+                  echo If 'psql' fails to connect, check logs at $PGDATA/log/
+
+                  export PATH="$PATH:scripts/path"
+                '';
+                # This adds `js-unknown-ghcjs-cabal` to the shell.
+                # shell.crossPlatforms = p: [p.ghcjs];
               };
-              # Non-Haskell shell tools go here
-              shell.buildInputs = with pkgs; [
-                ghcid
-                hpack
-                haskellPackages.brittany # brittany in shell.tools fails building
-                # hsPkgs.hspec-discover.components.exes.hspec-discover
-                postgres
-                glibcLocales
-                cacert
-                postgres-service
-              ];
-              shell.shellHook = ''
-                source scripts/source-env.sh .env
-
-                # init-postgres doesn't actually work with direnv. I tried to daemonize starting postgres but was not able
-                # to make it work. See https://github.com/direnv/direnv/issues/755
-                init-postgres
-
-                echo You should be able to start postgres with 'pg_ctl start' and use 'psql' to connect to it, and it will be independent from any your own system might have provided.
-                echo You just might have to run ./scripts/create-dev-db.sh and then 'codd.sh up' first to create database $PGDATABASE.
-                echo If 'psql' fails to connect, check logs at $PGDATA/log/
-
-                export PATH="$PATH:scripts/path"
-              '';
-              # This adds `js-unknown-ghcjs-cabal` to the shell.
-              # shell.crossPlatforms = p: [p.ghcjs];
-            };
-          })
+            })
         ];
 
         flake = pkgs.coddProject.flake {
