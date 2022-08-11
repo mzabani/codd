33e0b7e4d48debe0b3e292dfedf92a63a605feca
Marcelo Zabani <mzabani@gmail.com>
2022-08-11 17:47:42 -0300

    Support URI Encoded connection strings

--- src/Codd/Parsing.hs
+++ src/Codd/Parsing.hs
@@ -30,9 +30,11 @@ import           Control.Monad                  ( guard
                                                 , void
                                                 , when
                                                 )
+import           Control.Monad.Identity         ( Identity(runIdentity) )
 import           Control.Monad.Trans            ( MonadTrans
                                                 , lift
                                                 )
+import Control.Monad.Trans.Except (runExceptT, throwE)
 import           Control.Monad.Trans.Resource   ( MonadThrow(throwM) )
 import           Data.Attoparsec.Text           ( Parser
                                                 , anyChar
@@ -54,7 +56,7 @@ import           Data.Attoparsec.Text           ( Parser
 import qualified Data.Attoparsec.Text          as Parsec
 import qualified Data.Char                     as Char
 import           Data.Kind                      ( Type )
-import           Data.List                      ( nub )
+import           Data.List                      ( nub, sortOn )
 import           Data.Map                       ( Map )
 import qualified Data.Map                      as Map
 import           Data.Maybe                     ( listToMaybe
@@ -70,6 +72,7 @@ import           Data.Time.Clock                ( UTCTime(..) )
 import           Database.PostgreSQL.Simple     ( ConnectInfo(..) )
 import qualified Database.PostgreSQL.Simple.Time
                                                as DB
+import Network.URI (URI(..), URIAuth(..), unEscapeString, parseURI)
 import           Prelude                 hiding ( takeWhile )
 import           Streaming                      ( MFunctor(hoist)
                                                 , Of(..)
@@ -198,6 +201,8 @@ parseSqlPiecesStreaming
   => Stream (Of Text) m ()
   -> Stream (Of SqlPiece) m ()
 parseSqlPiecesStreaming contents = Streaming.concat parseResultsStream
+  -- NOTE: We might be able to simplify this function tremendously by using attoparsec's `parseWith`:
+  -- https://hackage.haskell.org/package/attoparsec-0.14.4/docs/Data-Attoparsec-Text.html#v:parseWith
  where
   mkErrorMsg errorMsg =
     "An internal parsing error occurred. This is most likely a bug in codd. Please file a bug report.\
@@ -468,6 +473,7 @@ blockEndingParser = \case
   SingleQuotedString     -> string "'"
 
 eol :: Parser Text
+-- TODO: Replace "eol" by endOfLine. "\t\n" is wrong!! It's "\r\n"!
 eol = string "\n" <|> string "\t\n"
 
 blockParser :: Parser (BlockType, Text)
@@ -534,45 +540,110 @@ parseWithEscapeCharProper untilc = do
       pure $ cs <> c <> rest
     Just _ -> pure cs
 
+eitherToMay :: Either a b -> Maybe b
+eitherToMay (Left _) = Nothing
+eitherToMay (Right v) = Just v
 
--- | Parses a string in the format postgres://username[:password]@host:port/database_name
+-- | Parses a URI with scheme 'postgres' or 'postgresql', as per https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING.
+-- The difference here is that URIs with a query string or with a fragment are not allowed.
+uriConnParser :: Text -> Either String ConnectInfo
+uriConnParser line = runIdentity $ runExceptT @String @_ @ConnectInfo $ do
+  case parseURI (Text.unpack line) of
+    Nothing -> throwE "Connection string is not a URI"
+    Just URI {..} -> do
+      when (Text.toLower (Text.pack uriScheme) `notElem` ["postgres:", "postgresql:"]) $ throwE "Connection string's URI scheme must be 'postgres' or 'postgresql'"
+      case uriAuthority of
+        Nothing -> throwE "Connection string must contain at least user and host"
+        Just URIAuth {..} -> do
+          let
+            connectDatabase = unEscapeString $ trimFirst '/' uriPath
+            hasQueryString = not $ null uriQuery
+            hasFragment = not $ null uriFragment
+          when (null connectDatabase) $ throwE "Connection string must contain a database name"
+          when (hasQueryString || hasFragment) $ throwE "Custom parameters are not supported in connection strings. Make sure your connection URI does not have a query string or query fragment"
+
+          -- Ports are not mandatory and are defaulted to 5432 when not present
+          let port = if null uriPort then Just 5432 else Nothing
+          case port <|> eitherToMay (parseOnly (Parsec.decimal <* endOfInput) (Text.pack $ trimFirst ':' uriPort)) of
+            Nothing -> throwE "Invalid port in connection string"
+            Just connectPort -> do
+              let (unEscapeString . trimLast '@' -> connectUser, unEscapeString . trimLast '@' . trimFirst ':' -> connectPassword) = break (== ':') uriUserInfo
+              pure ConnectInfo {
+                     connectHost = unEscapeString $ trimFirst '[' $ trimLast ']' $ uriRegName -- Handle IPv6
+                   , connectPort
+                   , connectUser
+                   , connectPassword
+                   , connectDatabase
+                   }
+
+  where
+    trimFirst :: Char -> String -> String
+    trimFirst c s@(c1 : cs) = if c == c1 then cs else s
+    trimFirst _ s = s
+
+    trimLast :: Char -> String -> String
+    trimLast c s =
+      case Text.unsnoc $ Text.pack s of
+        Nothing -> s
+        Just (t, lastChar) -> if lastChar == c then Text.unpack t else s
+
+
+keywordValueConnParser :: Text -> Either String ConnectInfo
+keywordValueConnParser line = runIdentity $ runExceptT $ do
+  kvs <- fmap (sortOn fst) $ parseOrFail (singleKeyVal `Parsec.sepBy` takeWhile1 (== ' ')) line "Invalid connection string"
+  -- TODO: Error if there are other keys that we're not using present.
+  ConnectInfo
+    <$> getVal "host" Nothing txtToString kvs
+    <*> getVal "port" (Just 5432) Parsec.decimal kvs
+    <*> getVal "user" Nothing txtToString kvs
+    <*> getVal "password" (Just "") txtToString kvs
+    <*> getVal "dbname" Nothing txtToString kvs
+  where
+    getVal key def parser pairs = case (map snd $ filter ((== key) . fst) pairs, def) of
+      ([], Nothing) -> throwE $ "Connection string must contain a value for '" <> Text.unpack key <> "'"
+      ([], Just v) -> pure v
+      (vt : [], _) -> parseOrFail parser vt $ "Connection string key '" <> Text.unpack key <> "' is in an unrecognizable format"
+      _ -> throwE $ "Duplicate key '" <> Text.unpack key <> "' found in connection string."
+
+    txtToString = Parsec.takeText >>= pure . Text.unpack
+
+    parseOrFail parser txt errorMsg =
+      case parseOnly (parser <* endOfInput) txt of
+        Left _ -> throwE errorMsg
+        Right v -> pure v
+
+    singleKeyVal = do
+      key <- takeWhile1 (\c -> c /= ' ' && c /= '=')
+      skipJustSpace
+      void $ char '='
+      skipJustSpace
+      -- Is "password=" the same as having password = Nothing? And does psql interpret that the same
+      -- as not having specified any password, as opposed to having a blank password?
+      -- TODO: Verify.
+      -- TODO: Test unquoted values that contain single quotes and backslashes with psql to make sure
+      -- we're getting this right.
+      value <- takeQuotedString <|> parseWithEscapeCharProper (== ' ') <|> pure ""
+      pure (key, value)
+
+    takeQuotedString = do
+      void $ char '\''
+      s <- parseWithEscapeCharProper (== '\'')
+      void $ char '\''
+      pure s
+
+-- | Parses a string in one of libpq allowed formats. See https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING.
+-- The difference here is that only a subset of all connection parameters are allowed.
 connStringParser :: Parser ConnectInfo
 connStringParser = do
-  void $ string "postgres://" <|> string "postgresql://"
-  usr <- idParser "username"
-  pwd <- (char ':' *> idParser "password") <|> pure ""
-  void $ char '@'
-  host <- ipv6Parser <|> idParser "host" <|> fail
-    "Failed parsing host in the connection string"
-  void $ char ':' <|> fail "Missing colon after host"
-  port <- Parsec.decimal
-    <|> fail "Could not find a port in the connection string."
-  void $ char '/'
-  adminDb <- idParser "database"
-  pure ConnectInfo { connectHost     = host
-                   , connectPort     = port
-                   , connectUser     = usr
-                   , connectPassword = pwd
-                   , connectDatabase = adminDb
-                   }
- where
-  idParser :: String -> Parser String
-  idParser idName = do
-    x <- Text.unpack
-      <$> parseWithEscapeCharProper (\c -> c == ':' || c == '@' || c == '\n')
-    when (x == "")
-      $  fail
-      $  "Could not find a "
-      <> idName
-      <> " in the connection string."
-    pure x
-
-  ipv6Parser :: Parser String
-  ipv6Parser = do
-    void $ char '['
-    x <- Text.unpack <$> parseWithEscapeCharProper (== ']')
-    void $ char ']'
-    pure x
+  connStr <- Parsec.takeWhile1 (not . Parsec.isEndOfLine) <|> fail "Empty connection string"
+  -- Very poor connection string type handling here
+  let
+    connStrParser = if ("postgres://" `Text.isPrefixOf` Text.toLower connStr) || ("postgresql://" `Text.isPrefixOf` Text.toLower connStr) then
+      uriConnParser
+    else keywordValueConnParser
+  case connStrParser connStr of
+    Left err -> fail err
+    Right c -> pure c
 
 
 optionParser :: Parser SectionOption
--- test/EnvironmentSpec.hs
+++ test/EnvironmentSpec.hs
@@ -22,6 +22,7 @@ import           Data.Time                      ( secondsToDiffTime )
 import           Data.Word                      ( Word16 )
 import           Database.PostgreSQL.Simple     ( ConnectInfo(..) )
 import GHC.Generics (Generic)
+import Network.URI (URI(..), URIAuth(..), escapeURIString, isUnescapedInURIComponent, uriToString)
 import           Test.Hspec
 import           Test.Hspec.Core.QuickCheck     ( modifyMaxSuccess )
 import           Test.QuickCheck
@@ -56,29 +57,28 @@ instance Arbitrary ConnStringType where
 
 getConnString
     :: ConnStringType -> ObjName -> PrintableString -> HostString -> Word16 -> ObjName -> Text
-getConnString connStrType (unObjName -> usr) (Text.pack . getPrintableString -> pwd) (Text.pack . unHostString -> host) (Text.pack . show -> port) (unObjName -> dbName)
-    = (case connStrType of
-            URIpostgres -> "postgres://"
-            URIpostgresql -> "postgresql://"
-        )
-        <> escapeConnStringPiece usr
-        <> (if pwd == "" then "" else ":" <> escapeConnStringPiece pwd)
-        <> "@"
-        <> escapeHost host
-        <> ":"
-        <> port
-        <> "/"
-        <> escapeConnStringPiece dbName
+getConnString connStrType (Text.unpack . unObjName -> usr) (getPrintableString -> pwd) (unHostString -> host) port (Text.unpack . unObjName -> dbName)
+    = Text.pack $ uriToString id uri ""
   where
+    uri = URI {
+        uriScheme = case connStrType of
+            URIpostgres -> "postgres:"
+            URIpostgresql -> "postgresql:"
+        , uriAuthority = Just URIAuth {
+            uriUserInfo = encodeURIComponent usr <> (if pwd == "" then "" else (':' : encodeURIComponent pwd)) <> "@"
+            , uriRegName = Text.unpack $ escapeHost $ Text.pack host
+            , uriPort = ':' : show port
+        }
+        , uriPath = '/' : encodeURIComponent dbName
+        , uriQuery = ""
+        , uriFragment = ""
+    }
+    encodeURIComponent :: String -> String
+    encodeURIComponent = escapeURIString isUnescapedInURIComponent
     escapeHost :: Text -> Text
     escapeHost host =
         -- bracket-escape IPv6 addresses
         if ":" `Text.isInfixOf` host then "[" <> host <> "]" else host
-    escapeConnStringPiece :: Text -> Text
-    escapeConnStringPiece =
-        Text.replace "@" "\\@" . Text.replace ":" "\\:" . Text.replace
-            "\\"
-            "\\\\"
 
 data ConnStringGen = ConnStringGen Text ConnectInfo
     deriving stock Show
@@ -107,8 +107,25 @@ spec :: Spec
 spec = do
     describe "Environment tests" $ do
         context "Connection string parsing" $ do
-            -- protocol://username[:password]@host:port/database_name
-            -- Escape chars such as :, @ and even \ with a backslash. E.g. \:, \@ and \\
+            it "Some hard coded URI Encoded connection strings" $ do
+                -- These are hard coded connection strings that I've tested with psql at some point
+                parseOnly (connStringParser <* endOfInput) "postgresql://postgres@localhost/some%20thing%20%2F%3F%3A%40"
+                        `shouldBe` Right ConnectInfo {
+                            connectHost = "localhost"
+                            , connectPort = 5432
+                            , connectUser = "postgres"
+                            , connectPassword = ""
+                            , connectDatabase = "some thing /?:@"
+                        }
+                parseOnly (connStringParser <* endOfInput) "postgresql://some%20thing%20%2F%3F%3A%40:passwdsome%20thing%20%2F%3F%3A%40@localhost:1/some%20thing%20%2F%3F%3A%40"
+                        `shouldBe` Right ConnectInfo {
+                            connectHost = "localhost"
+                            , connectPort = 1
+                            , connectUser = "some thing /?:@"
+                            , connectPassword = "passwdsome thing /?:@"
+                            , connectDatabase = "some thing /?:@"
+                        }
+            
             it "Full connection string with password" $ do
                 property $ \(ConnStringGen connStr connInfo) ->
                     parseOnly (connStringParser <* endOfInput) connStr
