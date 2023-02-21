011ca58bc9a5b4a890b7f8dc45fd9f49c382a645
Marcelo Zabani <mzabani@gmail.com>
2023-02-21 20:23:53 -0300

    A few more fixes

--- nix/prepare-cache-for-ci.sh
+++ nix/prepare-cache-for-ci.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 cachix watch-exec mzabani -- nix build --no-link ".#x86_64-unknown-linux-musl:codd:test:codd-test"
 cachix watch-exec mzabani -- nix build --no-link ".#x86_64-unknown-linux-musl:codd:lib:codd.doc"
 cachix watch-exec mzabani -- nix build --no-link ".#x86_64-unknown-linux-musl:codd:bench:codd-bench"
-cachix watch-exec mzabani -- nix-build --no-output-link ./nix/install-codd-nixpkgs.nix -A codd
+cachix watch-exec mzabani -- nix-build --no-out-link ./nix/install-codd-nixpkgs.nix -A codd
 cachix watch-exec mzabani -- nix build --no-link .#flakeAeson1.x86_64-linux.codd-musl
 cachix watch-exec mzabani -- nix build --no-link .#dockerImage.x86_64-linux
