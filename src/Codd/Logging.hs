3ae3c05db55a92f708464198c02c7119f2b74d86
Marcelo Zabani <mzabani@gmail.com>
2024-03-04 19:34:23 -0300

    Float color support value out. Add some comments

--- src/Codd/Logging.hs
+++ src/Codd/Logging.hs
@@ -34,27 +34,43 @@ import           UnliftIO                       ( Handle
                                                 , stdout
                                                 )
 
+{-|
+Our usage of monad-logger is a travesty. Codd calls `logInfoN` and other methods with string markers inside the message itself
+that are replaced by colours or are stripped away and interpreted as a signal not to add a newline when printing.
+
+The problem is that all of these string markers will be printed ipsis litteris by any other monad-logger that is not `runCoddLogger`,
+including for example `runStdoutLoggingT` and `runStderrLoggingT`, but also any custom monad-logger instances in the world out there.
+
+We could instead use ANSI color escape codes, but we don't want to include them in contexts where colors aren't supported, which is hard without a custom instance of monad-logger.
+
+One approach would be to require (MonadIO m, MonadLogger m) everywhere and make codd use new functions we create here, such as a
+`logCoddInfoN`. We could then detect support for colors and conditionally replace string markers.
+
+While this would be much cleaner, that MonadIO constraint would be overkill for some parts of our codebase.
+
+So the proper thing to do would probably be to stop using monad-logger altogether, and create our own `MonadLogger` class. 
+-}
 data Verbosity = Verbose | NonVerbose
 
-printLogMsg :: MonadIO m => Verbosity -> LogLevel -> LogStr -> Handle -> m ()
-printLogMsg v level (decodeUtf8 . fromLogStr -> msg) handle = liftIO $ do
-    suppColor <- hSupportsANSIColor stdout
-    let (printFunc, finalMsg) =
-            case unlikelyCharSequence `Text.stripPrefix` msg of
-                Just noNewLineMsg ->
-                    ( \t -> do
-                        Text.hPutStr handle $ colorReplace suppColor t
-                        hFlush handle
-                    , noNewLineMsg
-                    )
-                Nothing ->
-                    (Text.hPutStrLn handle . colorReplace suppColor, msg)
-    case (v, level) of
-        (NonVerbose, LevelDebug) -> pure ()
-        (NonVerbose, LevelInfo ) -> pure ()
-        (_, LevelWarn) -> printFunc $ "<YELLOW>Warn:</YELLOW> " <> finalMsg
-        (_         , LevelError) -> printFunc $ "<RED>Error:</RED> " <> finalMsg
-        _                        -> printFunc finalMsg
+printLogMsg
+    :: MonadIO m => Bool -> Verbosity -> LogLevel -> LogStr -> Handle -> m ()
+printLogMsg suppColor v level (decodeUtf8 . fromLogStr -> msg) handle =
+    liftIO $ do
+        let (printFunc, finalMsg) =
+                case unlikelyCharSequence `Text.stripPrefix` msg of
+                    Just noNewLineMsg ->
+                        ( \t -> do
+                            Text.hPutStr handle $ colorReplace t
+                            hFlush handle
+                        , noNewLineMsg
+                        )
+                    Nothing -> (Text.hPutStrLn handle . colorReplace, msg)
+        case (v, level) of
+            (NonVerbose, LevelDebug) -> pure ()
+            (NonVerbose, LevelInfo) -> pure ()
+            (_, LevelWarn) -> printFunc $ "<YELLOW>Warn:</YELLOW> " <> finalMsg
+            (_, LevelError) -> printFunc $ "<RED>Error:</RED> " <> finalMsg
+            _ -> printFunc finalMsg
 
   where
     cyan    = fromString $ setSGRCode [SetColor Foreground Dull Cyan]
@@ -63,7 +79,7 @@ printLogMsg v level (decodeUtf8 . fromLogStr -> msg) handle = liftIO $ do
     red     = fromString $ setSGRCode [SetColor Foreground Dull Red]
     yellow  = fromString $ setSGRCode [SetColor Foreground Dull Yellow]
     reset   = fromString $ setSGRCode [Reset]
-    colorReplace suppColor =
+    colorReplace =
         Text.replace "<CYAN>" (if suppColor then cyan else "")
             . Text.replace "</CYAN>" (if suppColor then reset else "")
             . Text.replace "<GREEN>" (if suppColor then green else "")
@@ -80,8 +96,11 @@ printLogMsg v level (decodeUtf8 . fromLogStr -> msg) handle = liftIO $ do
 -- when the terminal supports them for those. Also, by convention, parses special strings such as <GREEN>some text</GREEN>
 -- to use colors when the terminal supports them.
 runCoddLogger :: MonadIO m => Verbosity -> LoggingT m a -> m a
-runCoddLogger v m =
-    runLoggingT m (\_loc _source level msg -> printLogMsg v level msg stdout)
+runCoddLogger v m = do
+    suppColor <- liftIO $ hSupportsANSIColor stdout
+    runLoggingT
+        m
+        (\_loc _source level msg -> printLogMsg suppColor v level msg stdout)
 
 unlikelyCharSequence :: IsString a => a
 unlikelyCharSequence = "      -----$$$$$+++++%%%%%######@@@@@"
@@ -92,10 +111,12 @@ logInfoNoNewline msg = logInfoN $ unlikelyCharSequence <> msg -- We cheat and pr
 -- | Logs Errors only, and to stderr. Useful if some command needs to produce output to be consumed
 -- by other programs, such as JSON output, so that they can `putStrLn` themselves and not worry about
 -- anything else going to stdout.
-runErrorsOnlyLogger :: LoggingT m a -> m a
-runErrorsOnlyLogger m = runLoggingT
-    m
-    (\_loc _source level msg -> case level of
-        LevelError -> printLogMsg Verbose level msg stderr
-        _          -> pure ()
-    )
+runErrorsOnlyLogger :: MonadIO m => LoggingT m a -> m a
+runErrorsOnlyLogger m = do
+    suppColor <- liftIO $ hSupportsANSIColor stdout
+    runLoggingT
+        m
+        (\_loc _source level msg -> case level of
+            LevelError -> printLogMsg suppColor Verbose level msg stderr
+            _          -> pure ()
+        )
