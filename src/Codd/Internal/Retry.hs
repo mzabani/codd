c7fac6aa89386e1954ee164d5ee4e798b043f3aa
Marcelo Zabani <mzabani@gmail.com>
2024-03-09 22:00:10 -0300

    Don't group no-txn migs together so retrying the block means retrying a single migration.
    This invariant is not obvious and so fragile to rely on.
    
    This commit also includes a bit more of the plumbing that is necessary
    to retry no-txn migs appropriately, but there's still much to do.

--- src/Codd/Internal/Retry.hs
+++ src/Codd/Internal/Retry.hs
@@ -1,6 +1,5 @@
 module Codd.Internal.Retry
     ( RetryIteration(..)
-    , retry_
     , retryFold
     ) where
 
@@ -15,50 +14,54 @@ import           Data.Maybe                     ( isNothing )
 import qualified Data.Text                     as Text
 import           UnliftIO                       ( MonadUnliftIO )
 import           UnliftIO.Concurrent            ( threadDelay )
-import           UnliftIO.Exception             ( catchAny )
+import           UnliftIO.Exception             ( Exception
+                                                , catch
+                                                )
 
-data RetryIteration = RetryIteration
-    { isLastTry :: Bool
-    , tryNumber :: Int
+data RetryIteration a = RetryIteration
+    { isLastTry     :: Bool
+    , tryNumber     :: Int
     -- ^ 0-indexed try number. E.g. 0 is the first try, 1 is the first *retry*.
+    , lastException :: Maybe a
+    -- ^ If this is a retry, the exception that caused it
     }
-
-retry_ :: (MonadUnliftIO m, CoddLogger m) => RetryPolicy -> m c -> m c
-retry_ rpol f = retryFold rpol accf () (const f) where accf () _ = pure ()
 
 -- | Retries an action as many times and with wait intervals according
 -- to the supplied `RetryPolicy`, but only retries in case of synchronous
 -- exceptions. Provides fold-like behavior for an accumulator
 -- for each try, including the first one.
 retryFold
-    :: (MonadUnliftIO m, CoddLogger m)
+    :: forall e m b a
+     . (MonadUnliftIO m, CoddLogger m, Exception e)
     => RetryPolicy
-    -> (b -> RetryIteration -> m b)
+    -> (b -> RetryIteration e -> m b)
     -- ^ Accumulating function. This runs even for the first try.
     -> b
     -- ^ Initial value of the accumulator.
     -> (b -> m a)
-    -- ^ Action to retry. Any synchronous exceptions are caught and logged.
+    -- ^ Action to retry. Any exceptions of the chosen type are caught and logged as errors.
     -- Retries don't happen in case no exceptions are thrown.
     -> m a
-retryFold initialPol accf acc0 f = go initialPol acc0 0
+retryFold initialPol accf acc0 f = go initialPol acc0 0 Nothing
   where
-    go rpol previousAcc tryNumber = do
+    go rpol previousAcc tryNumber lastException = do
         let mNextPol = retryPolicyIterate rpol
-            thisIter =
-                RetryIteration { isLastTry = isNothing mNextPol, tryNumber }
+            thisIter = RetryIteration { isLastTry     = isNothing mNextPol
+                                      , tryNumber
+                                      , lastException
+                                      }
         thisAcc <- accf previousAcc thisIter
         case mNextPol of
             Nothing                    -> f thisAcc
-            -- UnliftIO's `catchAny` does not catch async exceptions,
-            -- which is what we want here.
-            Just (waitIfFail, nextPol) -> catchAny (f thisAcc) $ \e -> do
+            Just (waitIfFail, nextPol) -> catch (f thisAcc) $ \(ex :: e) -> do
                 let waitTimeMS :: Int =
                         truncate $ (realToFrac waitIfFail :: Float) * 1000
-                logError $ "Got SQL Error: " <> Text.pack (show e)
+                -- It would be more reasonable if this retryFold function didn't print anything, letting
+                -- its callers do that. Maybe in the future.
+                logError $ "Got SQL Error: " <> Text.pack (show ex)
                 logWarn
                     $  "Waiting "
                     <> Text.pack (show waitTimeMS)
                     <> "ms before next try"
                 threadDelay (1000 * waitTimeMS)
-                go nextPol thisAcc (tryNumber + 1)
+                go nextPol thisAcc (tryNumber + 1) (Just ex)
