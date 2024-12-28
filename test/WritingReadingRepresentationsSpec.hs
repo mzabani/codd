865abc7cc96884964b5fad0280d14fbc03ab0fb4
Marcelo Zabani <mzabani@gmail.com>
2024-12-28 11:55:41 -0300

    Add test for when ancestors have difficult permissions

--- test/WritingReadingRepresentationsSpec.hs
+++ test/WritingReadingRepresentationsSpec.hs
@@ -13,13 +13,14 @@ import Test.Hspec
 import Test.Hspec.Core.QuickCheck (modifyMaxSuccess)
 import Test.QuickCheck
 import TypesGen (DbRepsGen (..))
-import UnliftIO.Directory (doesDirectoryExist)
+import UnliftIO.Directory (createDirectoryIfMissing, doesDirectoryExist, emptyPermissions, getTemporaryDirectory, setOwnerExecutable, setOwnerReadable, setPermissions)
 
 spec :: Spec
 spec = do
   describe "Writing and reading representations" $ do
     it "persistRepsToDisk is inverse of readRepsFromDisk" $ do
       property $ \(DbRepsGen dbHashes) -> do
+        -- /dev/shm is shared memory so should be faster, if it exists (MacOS doesn't have it)
         shmExists <- doesDirectoryExist "/dev/shm"
         let baseFolder = if shmExists then "/dev/shm" else "/tmp"
         persistRepsToDisk dbHashes (baseFolder </> "inverse-test-sql-folder")
@@ -29,3 +30,17 @@ spec = do
         let diffs = schemaDifferences dbHashes readDbHashes
         diffs `shouldBe` Map.empty
         readDbHashes `shouldBe` dbHashes
+    modifyMaxSuccess (const 1)
+      $ it
+        "persistRepsToDisk works even when expected schema dir parent has difficult permissions"
+      $ do
+        property $ \(DbRepsGen dbHashes) -> do
+          tempDir <- getTemporaryDirectory
+          let expectedSchemaDir = tempDir </> "badfolder/badsubfolder/expected-schema"
+          createDirectoryIfMissing True expectedSchemaDir
+          let permsNoWrite = setOwnerReadable True $ setOwnerExecutable True emptyPermissions
+          setPermissions (tempDir </> "badfolder/badsubfolder") permsNoWrite
+          setPermissions (tempDir </> "badfolder") permsNoWrite
+          persistRepsToDisk @IO
+            dbHashes
+            expectedSchemaDir
