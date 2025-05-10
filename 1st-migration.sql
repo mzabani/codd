c2ca71613a5d4fc8efb449185eb8b764e740cfbc
Marcelo Zabani <mzabani@gmail.com>
2025-05-10 19:40:40 -0300

    Add a new `column_replacement_begin` function

--- 1st-migration.sql
+++ 1st-migration.sql
@@ -1,22 +1,9 @@
 CREATE TYPE experience2 AS ENUM ('intern', 'junior', 'senior');
 ALTER TABLE employee ADD COLUMN experience2 experience2;
-CREATE FUNCTION __bgmig_employee_set_experience2() RETURNS TRIGGER AS $$
-BEGIN
-  NEW.experience2 := CASE WHEN NEW.experience='master' THEN 'senior' ELSE NEW.experience::text::experience2 END;
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-CREATE TRIGGER __bgmig_employee_set_experience2_on_update
-    BEFORE UPDATE OF experience
-    ON employee
-    EXECUTE FUNCTION __bgmig_employee_set_experience2();
-CREATE TRIGGER __bgmig_employee_set_experience2_on_insert
-    BEFORE INSERT
-    ON employee
-    EXECUTE FUNCTION __bgmig_employee_set_experience2();
-SELECT codd_schema.background_migration_begin('change-experience', '1 seconds',
+SELECT codd_schema.column_replacement_begin('change-experience', '1 seconds',
 $$
 UPDATE employee SET experience2=CASE WHEN experience='master' THEN 'senior' ELSE experience::text::experience2 END
     WHERE employee_id=(SELECT employee_id FROM employee WHERE (experience IS NULL) <> (experience2 IS NULL) LIMIT 1);
 $$
+, 'employee', 'experience', 'experience2', $$CASE WHEN NEW.experience='master' THEN 'senior' ELSE NEW.experience::text::experience2 END$$
 );
--- codd-background-schema.sql
+++ codd-background-schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS codd_schema.background_migrations (name text not null primary key);
 -- | This function schedules a background migration that must be a valid plpgsql function body. It will until the last statement in that body returns a row count of 0 (zero), and then will stop running.
 -- TODO: Is there no better way? The command tag check would be very nice. Maybe we ask to return a single boolean if that's not possible?
-CREATE OR REPLACE FUNCTION codd_schema.background_migration_begin(background_migration_name text, cronSchedule text, sqlToRunPeriodically text, runInsideTxn boolean = true) RETURNS VOID AS $func$
+CREATE OR REPLACE FUNCTION codd_schema.background_migration_begin(background_migration_name text, cron_schedule text, plpgsql_to_run_periodically text, runInsideTxn boolean = true) RETURNS VOID AS $func$
 DECLARE
   temp_bg_func_name text := 'temp_bg_migration_' || background_migration_name;
 BEGIN
@@ -12,7 +12,7 @@ BEGIN
   -- Note that cron.job_run_details might not be turned on.. so we can't rely on it!
 
   -- Insert into table first so there are no side effects if this is a duplicate name
-  -- TODO: is all SQL executed inside a function atomic even outside a txn? Check.
+  -- TODO: is all SQL executed inside a function atomic even outside a txn? Check. And honor `runInsideTxn`
   INSERT INTO codd_schema.background_migrations (name) VALUES (background_migration_name);
 
   EXECUTE format($sqltext$
@@ -24,15 +24,44 @@ BEGIN
           GET DIAGNOSTICS affected_row_count = row_count;
           IF affected_row_count = 0 THEN
             -- TODO: Can unscheduling cancel the job and rollback the changes applied in the last run? Check.
+            -- TODO: Update codd_schema.background_migrations with a 'finished' status
             PERFORM cron.unschedule('%s');
           END IF;
           RETURN;
         END;
         $$ LANGUAGE plpgsql;
-      $sqltext$ , temp_bg_func_name, sqlToRunPeriodically, background_migration_name);
+      $sqltext$ , temp_bg_func_name, plpgsql_to_run_periodically, background_migration_name);
 
   -- TODO: Does pg_cron run its statements inside transactions? And how do we control the isolation level?
-  PERFORM cron.schedule(background_migration_name, cronSchedule, format('SELECT codd_schema.%I()', temp_bg_func_name));
+  PERFORM cron.schedule(background_migration_name, cron_schedule, format('SELECT codd_schema.%I()', temp_bg_func_name));
+END;
+$func$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION codd_schema.column_replacement_begin(background_migration_name text, cron_schedule text, plpgsql_to_run_periodically text, tablename text, oldcolname text, newcolname text, new_col_update_trigger_expr text, runInsideTxn boolean = true) RETURNS VOID AS $func$
+DECLARE
+  trigger_base_name text := '_codd_bgmig_' || tablename || '_replace_' || oldcolname || '_' || newcolname;
+BEGIN
+  -- TODO: Check for a bunch of things like the other function does
+  -- TODO: Check old and new column exist, tell user the new column must be created by them before calling this function
+  -- TODO: Scheduling before creating the triggers might make the schedule task run before the triggers are created and return 0? Seems unlikely but pg_cron does execute things in separate connections. Oh, in that case the column might not even exist!
+  --       So yeah, schedule with pg_cron AFTER creating the triggers! 
+  PERFORM codd_schema.background_migration_begin(background_migration_name, cron_schedule, plpgsql_to_run_periodically);
+  EXECUTE format($triggers$
+  CREATE FUNCTION %I() RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.%I := %s;
+    RETURN NEW;
+  END;
+  $$ LANGUAGE plpgsql;
+  CREATE TRIGGER %I
+      BEFORE UPDATE OF %I
+      ON %I
+      EXECUTE FUNCTION %I();
+  CREATE TRIGGER %I
+      BEFORE INSERT
+      ON %I
+      EXECUTE FUNCTION %I();
+  $triggers$, trigger_base_name, newcolname, new_col_update_trigger_expr, 'upd' || trigger_base_name, oldcolname, tablename, trigger_base_name, 'ins' || trigger_base_name, tablename, trigger_base_name);
 END;
 $func$ LANGUAGE plpgsql;
 
