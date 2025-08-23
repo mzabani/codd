6e50fb37deaf8b9cdfaa2fd4ee69239662db8be3
Marcelo Zabani <mzabani@gmail.com>
2025-08-23 09:18:15 -0300

    Binary search to find error - part 2

--- test/TypesGen.hs
+++ test/TypesGen.hs
@@ -77,8 +77,8 @@ genObjName =
       [(100, genLower), (5, genMixed)]
   where
     -- Docs: https://www.postgresql.org/docs/12/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS
-    validLowerFirstChars = "abcdefghijklmnopqrstuvxwyzçáé"
-    validUpperFirstChars = "ABCDEFGHIJKLMNOPQRSTUVXWYZÇÁÉ"
+    validLowerFirstChars = "abcdefghijklmnopqrstuvxwyzíóúñ_"
+    validUpperFirstChars = "ABCDEFGHIJKLMNOPQRSTUVXWYZÍÓÚñ_"
     validLowerOtherChars = validLowerFirstChars ++ "0123456789$"
     validUpperOtherChars = validUpperFirstChars ++ "0123456789$"
     genLower = do
